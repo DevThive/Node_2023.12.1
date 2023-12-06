52d9af6d999f56cf5891eb196d0e49e2f96982ef
데브티브 <thive8564@gmail.com>
2023-12-06 13:54:05 +0900

    add signup and signIn and token

--- src/repositoryies/users.repository.js
+++ src/repositoryies/users.repository.js
@@ -1,1 +1,44 @@
-export class UsersRepository {}
+import { prisma } from "../utils/prisma/index.js";
+export class UsersRepository {
+  signupUser = async (email, nickname, hash) => {
+    const signupUser = await prisma.users.create({
+      data: {
+        email,
+        nickname,
+        password: hash,
+      },
+    });
+    return signupUser;
+  };
+
+  //email 중복 확인
+  existsEmail = async (email) => {
+    const existsEmail = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return existsEmail;
+  };
+
+  // nickname 중복 확인
+  //   existsNickname = async (nickname) => {
+  //     const existsNickname = await prisma.users.findUnique({
+  //       where: {
+  //         nickname,
+  //       },
+  //     });
+  //     return existsNickname;
+  //   };
+
+  authLogin = async (email) => {
+    const authLogin = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return authLogin;
+  };
+}
